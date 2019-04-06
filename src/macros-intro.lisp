22d4e1411c3a4f8798d224776b7f2da4a9ea6410
Ram Vedam <rvedam@gmail.com>
2019-04-06 17:43:02 -0600

    Documentation of Code

--- src/macros-intro.lisp
+++ src/macros-intro.lisp
@@ -29,6 +29,31 @@ USAGE:
         ((m) 60)
         ((h) 3600)))))
 
+#|
+We can certainly do better. By simply moving the sleep-units into a macro,
+we can get rid of this need to quote the unit of measure and just simply execute
+the generated sleep call with the computed value as a constant
+|#
+(defmacro sleep-units (value unit)
+  `(sleep
+    (* ,value
+       ,(case unit
+         ((s) 1)
+         ((m) 60)
+         ((h) 3600)))))
+
+;; macroexpand the following example
+(sleep-units 5 m)
+
+;; even look at the disassembly
+;; on SBCL all constants are left-shifted by a bit (looks like they are multiplied by 2)
+(disassemble (lambda () (sleep-units 5 m)))
+
+#|
+Cost of above abstraction: units of measure no longer can be runtime dispatched.
+By making the simple change and generate the unit of time before generating the code
+to sleep we can preserve runtime dispatching when we need it without loss of efficiency.
+|#
 (defmacro unit-of-time (value unit)
   `(* ,value
        ,(case unit
@@ -39,18 +64,28 @@ USAGE:
 (defmacro sleep-units (value unit)
   `(sleep (unit-of-time ,value ,unit)))
 
-;; reference : LoL p. 45
+#|
+Code Reference: Let Over Lambda p. 45
+
+Creation of a new control structure nlet to create named lets like those in Scheme.
+
+Control Structure (p. 47) : A construct that doesn't follow the behavior of a function.
+it can change the control flow of a program when executed (in this case once expanded).
+|#
+
 (defmacro nlet (n letargs &body body)
   `(labels ((,n ,(mapcar #'car letargs)
               ,@body))
      (,n ,@(mapcar #'cadr letargs))))
 
+;; example of macro usage
 (defun nlet-fact (n)
   (nlet fact ((n n))
     (if (zerop n)
         1
         (* n (fact (- n 1))))))
 
+;; tail recursive version of the above code
 (defun nlet-fact-tr (n)
   (nlet fact ((n n)
               (acc 1))
@@ -58,6 +93,13 @@ USAGE:
         acc
         (fact (- n 1) (* acc n)))))
 
+#|
+"only use macros when functions won't do" (p. 47)
+Author points out that using macros make sense when:
+- you don't want to evaluate certain arguments
+- evaluate argumets out of order
+- evaluate more than once 
+|#
 (defmacro nif (expr pos zero neg)
   (let ((g (gensym)))
     `(let ((,g ,expr))
@@ -65,6 +107,8 @@ USAGE:
              ((zerop ,g) ,zero)
              (t ,neg)))))
 
+#|
+|#
 (defun g!-symbol-p (s)
   (and (symbolp s)
        (> (length (symbol-name s)) 2)
